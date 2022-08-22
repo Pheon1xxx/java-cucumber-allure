fbae6fba7ec931d988a8e9e90bde7b1fa98a1867
Vladislav Chuprina <vladislav.chuprina@gmail.com>
2022-08-22 10:40:44 +0300

    V1

---
+++ src/test/java/api/Article.java
@@ -0,0 +1,25 @@
+package api;
+
+import io.cucumber.messages.internal.com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.*;
+import java.util.List;
+
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class Article {
+
+    private List<String> categories;
+    private String content;
+    private String date;
+    private String lang;
+    private String layout;
+    private List<Object> tags;
+    private String title;
+    private String url;
+
+}
